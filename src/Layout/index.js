c59aa21694a4607656b2062c01c7707dddc6a44b
Richard H Dobeck <rdobeck@yahoo.com>
2021-03-15 11:21:03 -0700

    corrected api request to list all decks

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,35 +2,29 @@ import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
 import { listDecks } from "../utils/api/index";
-import { Switch, Route, useHistory } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import DecksList from "../Decks/DecksList";
 
 function Layout() {
-  const history = useHistory();
   const [allDecks, setAllDecks] = useState([]);
 
   useEffect(() => {
     const abortController = new AbortController();
     const signal = abortController.signal;
 
-    listDecks(signal).then(setAllDecks);
+    setAllDecks(listDecks(signal));
 
-    return () => abortController.error();
+    return () => abortController.abort();
   }, []);
 
   return (
     <>
       <Header />
       <div className="container">
-        <button
-          type="button"
-          className="btn btn-secondary"
-          onClick={history.push("/decks/new")}
-        >
-          Create Deck
-        </button>
-        <DecksList allDecks={allDecks} />
         <Switch>
+          <Route exact path="/">
+            <DecksList allDecks={allDecks} />
+          </Route>
           <Route>
             <NotFound />
           </Route>

d1983c2e7921138b0689b450be8221a646b56da3
Richard H Dobeck <rdobeck@yahoo.com>
2021-03-15 11:47:29 -0700

    moved routes for CreateDeck, ViewDeck, and StudyDeck to this component

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -4,6 +4,9 @@ import NotFound from "./NotFound";
 import { listDecks } from "../utils/api/index";
 import { Switch, Route } from "react-router-dom";
 import DecksList from "../Decks/DecksList";
+import CreateDeck from "../Decks/CreateDeck";
+import ViewDeck from "../Decks/ViewDeck";
+import StudyDeck from "../Decks/StudyDeck";
 
 function Layout() {
   const [allDecks, setAllDecks] = useState([]);
@@ -24,6 +27,15 @@ function Layout() {
         <Switch>
           <Route exact path="/">
             <DecksList allDecks={allDecks} />
+          </Route>
+          <Route path="decks/new">
+            <CreateDeck />
+          </Route>
+          <Route path="decks/:deckId">
+            <ViewDeck />
+          </Route>
+          <Route path="decks/:deckId/study">
+            <StudyDeck />
           </Route>
           <Route>
             <NotFound />

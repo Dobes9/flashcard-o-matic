22f74d9eb4bade3314f584e2d4c2297f120a9565
Richard H Dobeck <rdobeck@yahoo.com>
2021-03-15 12:59:19 -0700

    fleshed out inputs for form, created change handler for form, submit handler for form, cancel button returns to home screen

--- src/Decks/CreateDeck.js
+++ src/Decks/CreateDeck.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory, Link } from "react-router-dom";
+import createDeck from "../utils/api/index";
 
 export default function CreateDeck() {
   const history = useHistory();
+  const initialFormState = {
+    name: "",
+    description: "",
+  };
+  const [formData, setFormData] = useState({ ...initialFormState });
+  const handleFormChange = ({ target }) => {
+    setFormData({
+      ...formData,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+    createDeck(formData, signal);
+    setFormData(...initialFormState);
+    history.push("/decks/:deckId");
+  };
   return (
     <>
       <nav aria-label="breadcrumb">
@@ -16,7 +37,43 @@ export default function CreateDeck() {
         </ol>
       </nav>
       <h2>Create Deck</h2>
-      <form></form>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Name
+          <input
+            id="name"
+            name="name"
+            type="text"
+            value={formData.name}
+            placeholder="Deck Name"
+            onChange={handleFormChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            rows="5"
+            value={formData.description}
+            placeholder="Brief description of the deck"
+            onChange={handleFormChange}
+          />
+        </label>
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={() => {
+              setFormData({...initialFormState});
+              history.push("/");
+          }}
+        >
+          Cancel
+        </button>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </>
   );
 }

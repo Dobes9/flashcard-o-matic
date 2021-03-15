33fc4a0c0592d8c44bc2b423c9ccd49f0c8752b3
Richard H Dobeck <rdobeck@yahoo.com>
2021-03-15 09:13:13 -0700

    add create deck button, deckslist component, put notfound inside a route

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import { listDecks } from "../utils/api/index";
+import { Switch, Route, useHistory } from "react-router-dom";
+import DecksList from "../Decks/DecksList";
 
 function Layout() {
+  const history = useHistory();
+  const [allDecks, setAllDecks] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    listDecks(signal).then(setAllDecks);
+
+    return () => abortController.error();
+  }, []);
+
   return (
     <>
       <Header />
       <div className="container">
-        {/* TODO: Implement the screen starting here */}
-        <NotFound />
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={history.push("/decks/new")}
+        >
+          Create Deck
+        </button>
+        <DecksList allDecks={allDecks} />
+        <Switch>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
     </>
   );

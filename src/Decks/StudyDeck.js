8e28480e664966ee50f352268df6559dd1b3d945
Richard H Dobeck <rdobeck@yahoo.com>
2021-03-15 11:48:53 -0700

    removed route component, now in Layout

--- src/Decks/StudyDeck.js
+++ src/Decks/StudyDeck.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function StudyDeck() {
-    return (
-        <Route path="/decks/:deckId/study">
-            <nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                    <li className="breadcrumb-item"><Link to="/">Home</Link></li>
-                    <li className="breadcrumb-item"><Link to="/decks/:deckId">Deck Name</Link></li>
-                    <li className="breadcrumb-item active" aria-current="page">Study</li>
-                </ol>
-            </nav>
-            <h2>Deck Name: Study</h2>
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">Card X of Total</h5>
-                    <p className="card-text">Card front/back</p>
-                    <button>Flip</button>
-                    <button>Next</button>
-                </div>
-            </div>
-        </Route>
-    )
+  return (
+    <>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="breadcrumb-item">
+            <Link to="/decks/:deckId">Deck Name</Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            Study
+          </li>
+        </ol>
+      </nav>
+      <h2>Deck Name: Study</h2>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Card X of Total</h5>
+          <p className="card-text">Card front/back</p>
+          <button>Flip</button>
+          <button>Next</button>
+        </div>
+      </div>
+    </>
+  );
 }

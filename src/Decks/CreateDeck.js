07e29a8aa078e641565c6b1ebdbdf1898a09cc45
Richard H Dobeck <rdobeck@yahoo.com>
2021-03-15 11:22:11 -0700

    added breadcrumb, location for header and form. no work on form at this time

--- src/Decks/CreateDeck.js
+++ src/Decks/CreateDeck.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory, Link, Switch, Route } from "react-router-dom";
 
 export default function CreateDeck() {
-    const history = useHistory();
-    return (
-        <div>
-            <nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                    <li className="breadcrumb-item"><Link to="/">Home</Link></li>
-                    <li className="breadcrumb-item active" aria-current="page">Create Deck</li>
-                </ol>
-            </nav>
-            <h2>Create Deck</h2>
-            <form>
-
-            </form>
-        </div>
-    )
+  const history = useHistory();
+  return (
+    <Route path="/decks/new">
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            Create Deck
+          </li>
+        </ol>
+      </nav>
+      <h2>Create Deck</h2>
+      <form></form>
+    </Route>
+  );
 }

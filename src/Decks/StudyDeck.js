84611cf95d6959ccb320526230326b3d043e3983
Richard H Dobeck <rdobeck@yahoo.com>
2021-03-16 08:08:34 -0700

    added deckId state, useEffect to load selected deck, added some dynamic properties to names based on selected deck

--- src/Decks/StudyDeck.js
+++ src/Decks/StudyDeck.js
@@ -1,23 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useRouteMatch } from "react-router-dom";
+import { readDeck } from "../utils/api/index";
 
 export default function StudyDeck() {
+  const { deckId } = useRouteMatch();
+  const [selectedDeck, setSelectedDeck] = useState({});
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    setSelectedDeck(readDeck(deckId, signal));
+
+    return () => abortController.abort();
+  }, [deckId]);
+
   return (
-    <>
+    <div>
       <nav aria-label="breadcrumb">
         <ol className="breadcrumb">
           <li className="breadcrumb-item">
             <Link to="/">Home</Link>
           </li>
           <li className="breadcrumb-item">
-            <Link to="/decks/:deckId">Deck Name</Link>
+            <Link to={`/decks/${deckId}`}>{selectedDeck.name}</Link>
           </li>
           <li className="breadcrumb-item active" aria-current="page">
             Study
           </li>
         </ol>
       </nav>
-      <h2>Deck Name: Study</h2>
+      <h2>Study: {selectedDeck.name}</h2>
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">Card X of Total</h5>
@@ -26,6 +39,6 @@ export default function StudyDeck() {
           <button>Next</button>
         </div>
       </div>
-    </>
+    </div>
   );
 }

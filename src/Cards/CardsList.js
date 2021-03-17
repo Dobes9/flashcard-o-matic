bdf241b3d3a74ea3cae30af759d1ebdb69d2a68e
Richard H Dobeck <rdobeck@yahoo.com>
2021-03-17 09:22:52 -0700

    renders all cards in selected deck, edit and delete buttons function as intended

---
+++ src/Cards/CardsList.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useHistory, useRouteMatch } from "react-router-dom";
+import { deleteCard } from "../utils/api/index";
+
+export default function CardsList({ cardsInDeck }) {
+  const history = useHistory();
+  const { url } = useRouteMatch();
+  const listOfCards = cardsInDeck.map((card) => {
+    return (
+      <div className="card" key={card.id}>
+        <div className="card-body">
+          <div className="row">
+            <div className="col">
+              <p className="card-text">{card.front}</p>
+            </div>
+            <div className="col">
+              <p className="card-text">{card.back}</p>
+              <button
+                className="btn btn-secondary"
+                onClick={() => history.push(`${url}/cards/${card.id}/edit`)}
+              >
+                Edit
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => {
+                  const confirmDeleteCard = window.confirm(
+                    "Delete this card? \n \nYou will not be able to recover it."
+                  );
+                  if (confirmDeleteCard) {
+                    const abortController = new AbortController();
+                    deleteCard(card.id, abortController.signal);
+                  }
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  });
+  return <div>{listOfCards}</div>;
+}

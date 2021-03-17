7298b3e0498e701e74bc6e50983dcb3a1dc9dd7c
Richard H Dobeck <rdobeck@yahoo.com>
2021-03-16 18:48:01 -0700

    starting fresh with many files removed and Layout cleaned out

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -3,10 +3,6 @@ import Header from "./Header";
 import NotFound from "./NotFound";
 import { listDecks } from "../utils/api/index";
 import { Switch, Route } from "react-router-dom";
-import DecksList from "../Decks/DecksList";
-import CreateDeck from "../Decks/CreateDeck";
-import ViewDeck from "../Decks/ViewDeck";
-import StudyDeck from "../Decks/StudyDeck";
 
 function Layout() {
   const [allDecks, setAllDecks] = useState([]);
@@ -15,7 +11,12 @@ function Layout() {
     const abortController = new AbortController();
     const signal = abortController.signal;
 
-    setAllDecks(listDecks(signal));
+    async function loadAllDecks() {
+      const decksFromAPI = await listDecks(signal);
+      setAllDecks(decksFromAPI);
+    }
+
+    loadAllDecks();
 
     return () => abortController.abort();
   }, []);
@@ -25,18 +26,6 @@ function Layout() {
       <Header />
       <div className="container">
         <Switch>
-          <Route exact path="/">
-            <DecksList allDecks={allDecks} />
-          </Route>
-          <Route path="/decks/new">
-            <CreateDeck />
-          </Route>
-          <Route path="/decks/:deckId">
-            <ViewDeck />
-          </Route>
-          <Route path="/decks/:deckId/study">
-            <StudyDeck />
-          </Route>
           <Route>
             <NotFound />
           </Route>

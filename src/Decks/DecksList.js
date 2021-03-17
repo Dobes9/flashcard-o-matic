68b993717c13b70273e1f1c69948c992010b99be
Richard H Dobeck <rdobeck@yahoo.com>
2021-03-16 19:40:02 -0700

    refactored routes in Layout, fixed render of all decks, got some buttons to navigate to the proper page

--- src/Decks/DecksList.js
+++ src/Decks/DecksList.js
@@ -1,5 +1,48 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-export default function DecksList() {
-    
+export default function DecksList({ allDecks }) {
+  const history = useHistory();
+  const listOfDecks = allDecks
+    .filter((deck) => {
+      return deck.cards.length > 0;
+    })
+    .map((deck) => {
+      return (
+        <div className="card" key={deck.id}>
+          <div className="card-title">
+            <h4>{deck.name}</h4>
+            <p style={{ justifyContent: "right" }}>{deck.cards.length} cards</p>
+          </div>
+          <div className="card-body">
+            {deck.description}
+            <button
+              className="btn btn-secondary"
+              onClick={() => history.push(`/decks/${deck.id}`)}
+            >
+              View
+            </button>
+            <button
+              className="btn btn-primary"
+              onClick={() => history.push(`/decks/${deck.id}/study`)}
+            >
+              Study
+            </button>
+            <button
+              className="btn btn-danger"
+              style={{ justifyContent: "right" }}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      );
+    });
+
+  return (
+    <div>
+      <button className="btn btn-secondary">Create Deck</button>
+      {listOfDecks}
+    </div>
+  );
 }
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import DecksList from "../Decks/DecksList";
+import CreateDeck from "../Decks/CreateDeck";
+import ViewDeck from "../Decks/ViewDeck";
+import StudyDeck from "../Decks/StudyDeck";
 import { listDecks } from "../utils/api/index";
 import { Switch, Route } from "react-router-dom";
 
@@ -20,12 +24,25 @@ function Layout() {
 
     return () => abortController.abort();
   }, []);
+  
 
   return (
     <div>
       <Header />
       <div className="container">
         <Switch>
+          <Route exact path="/">
+            <DecksList allDecks={allDecks} />
+          </Route>
+          <Route path="/decks/new">
+            <CreateDeck />
+          </Route>
+          <Route path="/decks/:deckId/view">
+            <ViewDeck />
+          </Route>
+          <Route path="/decks/:deckId/study">
+            <StudyDeck />
+          </Route>
           <Route>
             <NotFound />
           </Route>

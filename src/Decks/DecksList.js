3350dc91f672adefee2b9676607c752317197b5a
Richard H Dobeck <rdobeck@yahoo.com>
2021-03-17 07:14:25 -0700

    added window.confirm to delete deck event handler

--- src/Decks/DecksList.js
+++ src/Decks/DecksList.js
@@ -1,47 +1,58 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { deleteDeck } from "../utils/api/index";
 
 export default function DecksList({ allDecks }) {
   const history = useHistory();
-  const listOfDecks = allDecks
-    .filter((deck) => {
-      return deck.cards.length > 0;
-    })
-    .map((deck) => {
-      return (
-        <div className="card" key={deck.id}>
-          <div className="card-title">
-            <h4>{deck.name}</h4>
-            <p style={{ justifyContent: "right" }}>{deck.cards.length} cards</p>
-          </div>
-          <div className="card-body">
-            {deck.description}
-            <button
-              className="btn btn-secondary"
-              onClick={() => history.push(`/decks/${deck.id}`)}
-            >
-              View
-            </button>
-            <button
-              className="btn btn-primary"
-              onClick={() => history.push(`/decks/${deck.id}/study`)}
-            >
-              Study
-            </button>
-            <button
-              className="btn btn-danger"
-              style={{ justifyContent: "right" }}
-            >
-              Delete
-            </button>
-          </div>
+  const abortController = new AbortController();
+  const listOfDecks = allDecks.map((deck) => {
+    return (
+      <div className="card" key={deck.id}>
+        <div className="card-title">
+          <h4>{deck.name}</h4>
+          <p style={{ justifyContent: "right" }}>{deck.cards.length} cards</p>
         </div>
-      );
-    });
+        <div className="card-body">{deck.description}</div>
+        <div>
+          <button
+            className="btn btn-secondary"
+            onClick={() => history.push(`/decks/${deck.id}`)}
+          >
+            View
+          </button>
+          <button
+            className="btn btn-primary"
+            onClick={() => history.push(`/decks/${deck.id}/study`)}
+          >
+            Study
+          </button>
+          <button
+            className="btn btn-danger"
+            style={{ justifyContent: "right" }}
+            onClick={() => {
+              const confirmDeleteDeck = window.confirm(
+                "Delete this deck? \n \nYou will not be able to recover it."
+              );
+              if (confirmDeleteDeck) {
+                deleteDeck(deck.id, abortController.signal);
+              }
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
-      <button className="btn btn-secondary">Create Deck</button>
+      <button
+        className="btn btn-secondary"
+        onClick={() => history.push("/decks/new")}
+      >
+        Create Deck
+      </button>
       {listOfDecks}
     </div>
   );
